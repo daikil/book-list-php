e87dd78b6c962bea41eb6824f6a33b76363b37ba
daiki oikawa <daiki404@icloud.com>
2024-09-04 13:29:08 +0900

    step03

--- app/htdocs/book.php
+++ app/htdocs/book.php
@@ -1,4 +1,5 @@
 <?php
 declare(strict_types=1);
-
+require_once(dirname(__DIR__) . "/library/database_access.php");
+$data = DatabaseAccess::fetchAll();
 require_once(dirname(__DIR__) . "/template/book.php");
